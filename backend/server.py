d3dbc5a6e176bb1169b5ffdf2192e0481be78c37
cyrage52 <realcyrage@gmail.com>
2026-05-20 00:56:27 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -17,9 +17,10 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.getenv("MONGO_URL")
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+
+db = client[os.getenv("DB_NAME", "test")]
 
 # Resend configuration
 resend.api_key = os.environ['RESEND_API_KEY']
